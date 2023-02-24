f9712f56d337624d739ec7a044d398c293394a72
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-24 13:23:55 +0600

    navmenu added

---
+++ components/constants/index.js
@@ -0,0 +1,89 @@
+export const womenMenu = [
+  {
+    id: '/ladies-gown',
+    title: 'Ladies Gown',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/lades-shrug',
+    title: 'Ladies Shrug',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/slit-gown',
+    title: 'Slit Gown',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+]
+
+export const menMenu = [
+  {
+    id: '/tee-shirts-men',
+    title: 'T-shirts',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/sailor-shirts-men',
+    title: 'Sailor Shorts',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/sweatshirts-hoodies-&-knitwear-men',
+    title: 'Sweatshirts, Hoodies & Knitwear',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/shirts-&-polo-shirts-men',
+    title: 'Shirts & Polo Shirts',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/jackets-men',
+    title: 'Jackets',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/pants-&-shorts-pants-men',
+    title: 'Pants & Short Pants',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/outfits-sets-men',
+    title: 'Outfits Sets',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/accessories-men',
+    title: 'Accessories',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+]
+export const panjabiMenu = [
+  {
+    id: '/premium-panjabi',
+    title: 'Premium Panjabi',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/platinum-panjabi',
+    title: 'Platinum Panjabi',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/sahara-panjabi',
+    title: 'Sahara Panjabi',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+]
+export const paijamaMenu = [
+  {
+    id: '/paijama',
+    title: 'Paijama',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+  {
+    id: '/joggers',
+    title: 'Joggers',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, officia.',
+  },
+]
