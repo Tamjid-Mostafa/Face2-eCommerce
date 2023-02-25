f05d07f3c13bfd56cb1bd1f70d3f46ed3375c74e
xbayazid <bayazid9006@gmail.com>
2023-02-25 18:22:28 +0600

    waistcoat section completed

--- components/HomeWaistcoat.jsx
+++ components/HomeWaistcoat.jsx
@@ -5,7 +5,7 @@ export default function HomeWaistcoat() {
     return (
         <div className='h-[500px] hover:border-[1px] duration-200 ease-in hover:border-black'>
             <Link href="">
-                <img className='h-full w-full' src="https://i.ibb.co/QCN3nsd/men.jpg" alt="" />
+                <img className='h-full w-full' src="https://i.ibb.co/JnjZ13d/mens-suit-vest-guide-1080x675.jpg" alt="" />
                 <div className='-mt-20 ml-7'>
                     <h2 className='uppercase font-bold text-white'>Waistcoat Collection</h2>
                 </div>
