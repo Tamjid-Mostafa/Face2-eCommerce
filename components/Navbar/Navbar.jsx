eec41acbdbcb619d1e7909f1256ba7dd962602d6
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-26 23:07:30 +0600

    Navbar customization

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -13,17 +13,16 @@ import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import NavMenu from './NavMenu'
 import PopCart from '../PopCart'
-import { useSignInModal } from '../sign-in-modal'
 import ScrollLockNav from './ScrollLockNav'
 
 const Navbar = () => {
     const scrolled = useScroll(200)
     const [search, setSearch] = useState()
-    const { SignInModal, setShowSignInModal } = useSignInModal()
+
     return (
         <div
         // className='fixed top-0 w-full z-30 transition-all'
-        ><SignInModal />
+        >
 
             {/*  <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100 -z-10" />
             <HeaderMini /> */}
@@ -43,7 +42,7 @@ const Navbar = () => {
 
                         <div className="">
                             <p className="text-2xl font-bold text-primary">face2.com.bd</p>
-                            <p className="font-poppins text-sm font-semibold text-gray-700">
+                            <p className="font-poppins text-sm font-semibold ">
                                 YOUR FASHION OUR CONCERN
                             </p>
                         </div>
@@ -85,7 +84,7 @@ const Navbar = () => {
                         <AnimatePresence>
                             <Tooltip content="Wish List">
                                 <motion.button
-                                    onClick={() => setShowSignInModal(true)}
+
                                     className='rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black cursor-default outline-none'
                                     {...FADE_IN_ANIMATION_SETTINGS}>
                                     <HeartIcon />
