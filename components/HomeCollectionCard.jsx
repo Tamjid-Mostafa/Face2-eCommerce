1d36ab16a5fde7087d7e1b835d7ade6507a0745b
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-28 20:48:56 +0600

    changes className

--- components/HomeCollectionCard.jsx
+++ components/HomeCollectionCard.jsx
@@ -9,12 +9,12 @@ export default function HomeCollectionCard({ collection }) {
                 backgroundImage: `url(${image})`
             }}>
                 < Link href="" >
-                    <div class="bg-black bg-opacity-0  px-10 flex flex-wrap flex-col pt-[555px] hover:bg-opacity-40  transform duration-300">
+                    <div className="bg-black bg-opacity-0  px-10 flex flex-wrap flex-col pt-[555px] hover:bg-opacity-40  transform duration-300">
                         <div className='hidden group-hover:block'>
-                            <h1 class="text-white text-3xl mb-5 transform font-semibold uppercase translate-y-20 group-hover:translate-y-0 duration-300">
+                            <h1 className="text-white text-3xl mb-5 transform font-semibold uppercase translate-y-20 group-hover:translate-y-0 duration-300">
                                 {name}
                             </h1>
-                            <div class="w-16 h-2 bg-yellow-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
+                            <div className="w-16 h-2 bg-yellow-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
                             </div>
                         </div>
                     </div>
