5d7db59f0d0cf41467d0fac09668386f2c3e153d
rakib4485 <mdrakibulislam4485@gmail.com>
2023-02-28 20:57:29 +0600

    all product page done

--- pages/AllProducts.js
+++ pages/AllProducts.js
@@ -11,8 +11,8 @@ const AllProducts = () => {
     }, []);
     return (
         <div>
-            <h1 className="text-center text-3xl font-semibold mt-10 uppercase">Panjabi</h1>
-            <hr  className='my-5'/>
+            <h1 className="text-center text-3xl font-semibold my-10 uppercase">Panjabi</h1>
+            {/* <hr  className='my-5'/> */}
 
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
