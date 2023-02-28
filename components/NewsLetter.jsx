552a7a7517405a00e74fc8bfff9c4b3300f00daa
rakib4485 <mdrakibulislam4485@gmail.com>
2023-02-28 18:46:50 +0600

    swiperjs implement

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -6,9 +6,9 @@ import Link from 'next/link';
 const NewsLetter = () => {
     const [open, setOpen] = React.useState(false);
     return (
-        <div className='mx-[10%] my-10 bg-white border-4 py-12 duration-700'>
+        <div className='md:mx-[10%] my-10 bg-white border-4 py-12 duration-700'>
             <h2 className="text-center text-blackA10 font-semibold text-3xl uppercase tracking-wide">NewsLetter</h2>
-            <p className="text-slate-500 text-center text-lg py-4 bg-slate-200 mt-5">Keep yourself updated with the latest Sailor News, Fashion Updates and Blogs! Subscribe here! It’s simple</p>
+            <p className="text-slate-500 text-center text-lg py-4 bg-slate-200 mt-5">Keep yourself updated with the latest Face2 News, Fashion Updates and Blogs! Subscribe here! It’s simple</p>
             <Collapsible.Root className="w-full duration-700" open={open} onOpenChange={setOpen}>
                 <Collapsible.Content>
                     <div className='flex justify-center border-2 w-[350px] mx-auto mt-10 pl-4 duration-700'>
@@ -20,13 +20,13 @@ const NewsLetter = () => {
                 <div className='flex justify-center mt-5'>
 
                     <Collapsible.Trigger asChild>
-                        <button className="">
+                        <div className="">
                             {open ?
-                                <button className='py-2  px-10 font-semibold border-2 mx-auto hover:bg-black hover:text-white duration-500'>Subscribe </button>
+                                <button className='py-2  px-10 font-semibold border-2 bg-black text-white hover:bg-white hover:text-black duration-500'>Subscribe </button>
                                 : <span className="text-center mt-10">
-                                    <button className='py-2 px-10 font-semibold border-2 hover:bg-black hover:text-white duration-500'>Subscribe </button>
+                                    <button className='py-2 px-10 font-semibold border-2 bg-black text-white hover:bg-white hover:text-black duration-500'>Subscribe </button>
                                 </span>}
-                        </button>
+                        </div>
                     </Collapsible.Trigger>
                 </div>
 
